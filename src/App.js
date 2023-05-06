ec82202390cd77c082cbfb3af7bb59910f33da71
Dheeraj3018 <vsdheeraj17@gmail.com>
2023-05-06 13:37:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,43 +1,41 @@
 //import logo from './logo.svg';
-import './css/sb-admin-2.css';
-import Dashboard from './Dashboard';
-import Sidebar from './Sidebar';
-import Topbar from './Topbar';
-import Users from './Users';
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Createuser from './Createuser';
-import Login from './Login';
-import Forgotpassword from './Forgotpassword';
+import "./css/sb-admin-2.css";
+import Dashboard from "./Dashboard";
+import Sidebar from "./Sidebar";
+import Topbar from "./Topbar";
+import Users from "./Users";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Createuser from "./Createuser";
+import Login from "./Login";
+import Forgotpassword from "./Forgotpassword";
 
 function App() {
   return (
     <BrowserRouter>
-    {/* <!-- Page Wrapper --> */}
-    <div id="wrapper">
-      <Sidebar></Sidebar>
-      {/* <!-- Content Wrapper --> */}
-      <div id="content-wrapper" className="d-flex flex-column">
-        {/* <!-- Main Content --> */}
-        <div id="content">
-          <Topbar></Topbar>
-          <Routes>
-            <Route path='/Dashboard' element={<Dashboard />}/>
-            <Route path='/Users' element={<Users />}/>
-            <Route path='/Create-User' element={<Createuser/>}/>
-            <Route path='/Login' element={<Login></Login>}/>
-            <Route path='/Forgotpassword' element={<Forgotpassword></Forgotpassword>}/>
-          </Routes>
-         
+      {/* <!-- Page Wrapper --> */}
+      <div id="wrapper">
+        <Sidebar></Sidebar>
+        {/* <!-- Content Wrapper --> */}
+        <div id="content-wrapper" className="d-flex flex-column">
+          {/* <!-- Main Content --> */}
+          <div id="content">
+            <Topbar></Topbar>
+            <Routes>
+              <Route path="/Dashboard" element={<Dashboard />} />
+              <Route path="/Users" element={<Users />} />
+              <Route path="/Create-User" element={<Createuser />} />
+              <Route path="/Login" element={<Login></Login>} />
+              <Route
+                path="/Forgotpassword"
+                element={<Forgotpassword></Forgotpassword>}
+              />
+            </Routes>
+          </div>
+          {/* <!-- End of Main Content --> */}
         </div>
-        {/* <!-- End of Main Content --> */}
+        {/* <!-- End of Content Wrapper --> */}
       </div>
-      {/* <!-- End of Content Wrapper --> */}
-    </div>
-    {/* <!-- End of     Page Wrapper --> */}
+      {/* <!-- End of     Page Wrapper --> */}
     </BrowserRouter>
   );
 }
--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -28,15 +28,15 @@ function Dashboard (){
         }
     ]
     return(
-        //<!-- Begin Page Content -->
+       
                 <div className="container-fluid">
-                  {/* <!-- Page Heading --> */}
+                 
                     <div className="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
-                        <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
+                       <i
+                                className="fas fa-download fa-sm text-white-50"></i> Generate Report
                     </div>
-                    {/* <!-- Content Row --> */}
+               
                     <div className="row">
                         {
                             cards.map((card)=> {
@@ -47,7 +47,7 @@ function Dashboard (){
                         }
                     </div>
                 </div>
-        //<!-- /.container-fluid -->        
+         
     )
 }
 export default Dashboard
